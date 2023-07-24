0f577bd74843b5c68a1a7ff039763436e1ae233e
Greg Sterndale <gsterndale@gmail.com>
2023-07-24 07:08:06 -0400

    Mock EDGAR requests in unit tests

--- test/edgar.test.ts
+++ test/edgar.test.ts
@@ -1,12 +1,62 @@
 import { describe, expect, test } from "@jest/globals";
 import { EDGAR } from "../src/edgar";
+import fs from "fs";
+import path from "path";
 
 EDGAR.rateLimit = 10000000;
+
+type Path = string;
+type URLPattern = RegExp | string;
+type FixtureOptions = {
+  json?: Function | object;
+  text?: Function | string;
+  jsonPath?: Path;
+  textPath?: Path;
+};
+const ogFetch = global.fetch;
+const mockedFetches: { [key: string]: any } = {};
+const mockFetch = (
+  pattern: URLPattern,
+  fixture: FixtureOptions = {},
+  options: ResponseInit = {}
+) => {
+  var body: BodyInit | string | null = null;
+
+  if (fixture.jsonPath !== undefined)
+    body = fs
+      .readFileSync(path.resolve(__dirname, fixture.jsonPath as string))
+      .toString();
+
+  if (fixture.textPath !== undefined)
+    body = fs
+      .readFileSync(path.resolve(__dirname, fixture.textPath as string))
+      .toString();
+
+  mockedFetches[pattern.toString()] = {
+    pattern: pattern,
+    response: new Response(body, options),
+  };
+};
+
+global.fetch = jest.fn((url: string, init: RequestInit = {}) => {
+  const mockedFetch = Object.values(mockedFetches).find((mockedFetch) => {
+    return typeof mockedFetch.pattern === "string"
+      ? url === mockedFetch.pattern
+      : url.match(mockedFetch.pattern);
+  });
+  if (mockedFetch) {
+    return Promise.resolve(mockedFetch.response);
+  } else {
+    console.warn(`No matching pattern for: ${url}`);
+    return ogFetch(url, init);
+  }
+}) as jest.Mock;
 
 describe("finding a company's CIK by name", () => {
   let edgar: EDGAR;
 
   beforeEach(() => {
+    mockFetch(/company=Salesforce.*atom/, { textPath: "salesforce.xml" });
     edgar = new EDGAR();
   });
 
@@ -25,6 +75,7 @@ describe("finding new filings for a company", () => {
   });
 
   test("is not empty for a company by name", async () => {
+    mockFetch(/company=Salesforce.*atom/, { textPath: "salesforce.xml" });
     let company: Company = {
       name: "Salesforce, Inc.",
       filings: [],
@@ -37,6 +88,7 @@ describe("finding new filings for a company", () => {
   });
 
   test("is not empty for a company by CIK", async () => {
+    mockFetch(/CIK0001108524.json/, { jsonPath: "CIK0001108524.json" });
     let company: Company = {
       name: "Salesforce, Inc.",
       cik: "0001108524",
@@ -50,6 +102,7 @@ describe("finding new filings for a company", () => {
   });
 
   test("does not include filings already known", async () => {
+    mockFetch(/CIK0001108524.json/, { jsonPath: "CIK0001108524.json" });
     const number = "0001127602-23-019366";
     let company: Company = {
       name: "Salesforce, Inc.",
