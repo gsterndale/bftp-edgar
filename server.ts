17405fecfa007fea8ae03133a9d5e2437c23ae0f
Greg Sterndale <gsterndale@gmail.com>
2023-11-18 11:37:21 -0500

    /healthz always responds with 200 OK

--- server.ts
+++ server.ts
@@ -22,6 +22,10 @@ app.get("/filings", async (req: Request, res: Response) => {
   }
 });
 
+app.get("/healthz", (req: Request, res: Response) => {
+  return res.sendStatus(200);
+});
+
 const port = 3000; // Change this to the desired port number
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
